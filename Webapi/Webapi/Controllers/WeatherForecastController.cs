58097789a0e3ad70592f3837759107ca4cbd51ea
dinhthanhtungr@gmail.com <dinhthanhtungr@gmail.com>
2025-02-06 08:51:36 +0700

    Update

--- Webapi/Webapi/Controllers/WeatherForecastController.cs
+++ Webapi/Webapi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Webapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Rem"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
